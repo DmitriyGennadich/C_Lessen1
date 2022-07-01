47f7322cf640140a4332b137385b85c7a902844f
Dmitriy <fpmt2010@rambler.ru>
2022-07-01 22:46:01 +0300

    add Ex010

---
+++ Ex010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 12, 23, 27, 15, 16, 27, 68 };
+ int n = array.Length;
+ int find = 27;
+ int index = 0;
+ while(index < n)
+ {
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
