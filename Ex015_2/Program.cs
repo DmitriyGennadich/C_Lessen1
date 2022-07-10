7c4142e49402e44290cc0de3e1589ef6ed1f3e71
Dmitriy <fpmt2010@rambler.ru>
2022-07-10 18:59:36 +0300

    add Ex

---
+++ Ex015_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 0
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}
+
